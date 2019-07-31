eb48acc329b4e422e923ee92df7389e80cd63f0e
vivxz <viviannquan@gmail.com>
2019-07-31 16:22:12 -0700

    passing all store tests

--- src/store/store.js
+++ src/store/store.js
@@ -4,3 +4,21 @@ import rootReducer from './../reducers/main.js';
 import exampleVideoData from '../data/exampleVideoData.js';
 
 // TODO:  Create your redux store, apply thunk as a middleware, and export it!
+//a thunk is a function that returns another function that takes parameters : dispatch & getState
+
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunk)
+);
+
+// function anActionFunction(something){
+//   return function(dispatch) {
+//     /*
+//     return  someAsycFunction().then(
+//       someProp or action? => dispatch(actualActionFunction(prop, prop)),
+//       error => dispatch)(errorFunction(something,something, err))
+//     )
+//     */
+//   }
+// }
+export default store;
