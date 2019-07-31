50a09e6bb12ad3ba36ce36848847926eab0b0b6d
vivxz <viviannquan@gmail.com>
2019-07-31 15:35:10 -0700

    passing all currentVideo Reducers tests

--- src/actions/search.js
+++ src/actions/search.js
@@ -5,8 +5,11 @@ import YOUTUBE_API_KEY from '../config/youtube.js';
 
 
 var handleVideoSearch = (q) => {
- 
+
   //TODO:  Write an asynchronous action to handle a video search!
+  // var object = type?
+  // searchYouTube(object, cb) --> returns an array of item; assign it to changeVideoList & videos,
 };
 
 export default handleVideoSearch;
+
