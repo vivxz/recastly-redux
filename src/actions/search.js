5cb07b4ee36b150154c8efb10fffc69518da778e
vivxz <viviannquan@gmail.com>
2019-07-31 17:32:08 -0700

    working on handleVideoSearch

--- src/actions/search.js
+++ src/actions/search.js
@@ -7,7 +7,16 @@ import YOUTUBE_API_KEY from '../config/youtube.js';
 var handleVideoSearch = (q) => {
 
   //TODO:  Write an asynchronous action to handle a video search!
-  // var object = type?
+  var object = {YOUTUBE_API_KEY, q}
+  // var results = searchYouTube(object, (videos) => (changeVideoList(videos)))
+  // var result = searchYouTube(object, (video) => (changeVideo(video)))
+
+  // const mapDispatchToProps = dispatch => {
+  //   return {
+  //     changeVideoList: (results) => dispatch({ type: "CHANGE_VIDEO_LIST"}),
+  //     changeVideo: (result) => dispatch({type: 'CHANGE_VIDEO'})
+  //   }
+  // }
   // searchYouTube(object, cb) --> returns an array of item; assign it to changeVideoList & videos,
 };
 
--- src/containers/SearchContainer.js
+++ src/containers/SearchContainer.js
@@ -1,12 +1,27 @@
 import { connect } from 'react-redux';
 import Search from './../components/Search.js';
 import handleSearchChange from '../actions/search.js';
+import store from '../store/store.js';
 
-var SearchContainer = () => {};
+var SearchContainer = () => {
+  /*
+  switch statement that
+  returns results for different action cases
+  */
+};
 
 //TODO: define a SearchContainer component which will hook up your action
 // dispatchers with your search component props.
 //HINT: use react-redux 'connect' method to generate a container component from
 //state and dispatch mappings.
+const mapStateToProps = state => ({
+})
 
-export default SearchContainer;
+const mapDispatchToProps = dispatch => ({
+  handleVideoSearch: id => dispatch(handleVideoSearch(id))
+})
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Search);
